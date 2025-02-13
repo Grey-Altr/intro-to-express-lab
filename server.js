06af0508a7ca9089bad77c08839cd9817919ea9e
GreyA <grey.altaer@gmail.com>
2025-02-13 14:04:34 -0800

    exercise 4 complete

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const collectibles = [
     { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
 ];
 
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" },
+];
+
+
 
 // 1.
 
@@ -37,7 +48,20 @@ app.get('/collectibles/:index', (req, res) => {
 
 // 4.
 
-
+app.get('/shoes', (req, res) => {
+    if (req.query['min-price']) {
+        const expensiveShoes = shoes.filter(shoe => shoe.price > req.query['min-price']);
+        res.send(expensiveShoes);
+    } else if (req.query['max-price']) {
+        const cheapShoes = shoes.filter(shoe => shoe.price < req.query['max-price']);
+        res.send(cheapShoes);
+    } else if (req.query.type) {
+        const shoesOfAType = shoes.filter(shoe => shoe.type === req.query.type);
+        res.send(shoesOfAType);
+    } else {
+        res.send(shoes);
+    };
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
