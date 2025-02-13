66e795193c0702ca11885ebd4cb772ede5e65809
GreyA <grey.altaer@gmail.com>
2025-02-13 13:07:22 -0800

    exercise 2 complete

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ app.get('/greetings/:username', (req, res) => {
     res.send(`Hello there, ${req.params.username}`);
 });
 
+// 2.
+
+app.get('/roll/:number', (req, res) => {
+    if (isNaN(req.params.number)) {
+        res.send('You must specify a number!');
+    } else {
+        let randomNumber = Math.floor(Math.random() * req.params.number);
+        res.send(`You rolled a ${randomNumber}!`);
+    };
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
