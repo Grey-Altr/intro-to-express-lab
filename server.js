8cc7851c7f0df127632e5fdd9eafe5b9b67686de
GreyA <grey.altaer@gmail.com>
2025-02-13 13:22:26 -0800

    exercise 3 complete

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const collectibles = [
+    { name: "shiny ball", price: 5.95 },
+    { name: "autographed picture of a dog", price: 10 },
+    { name: "vintage 1970s yogurt SOLD AS-IS", price: 0.99 },
+];
+
+
 // 1.
 
 app.get('/greetings/:username', (req, res) => {
@@ -15,6 +22,16 @@ app.get('/roll/:number', (req, res) => {
     } else {
         let randomNumber = Math.floor(Math.random() * req.params.number);
         res.send(`You rolled a ${randomNumber}!`);
+    };
+});
+
+// 3.
+
+app.get('/collectibles/:index', (req, res) => {
+    if (!collectibles[req.params.index]) {
+        res.send('This item is not yet in stock. Check back soon!');
+    } else {
+        res.send(`So, you want the ${collectibles[req.params.index].name}? For ${[req.params.index].price}, it can be yours!`);
     };
 });
 
